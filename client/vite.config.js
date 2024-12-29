40b55a1b8dc850dc1f320bc85153131c66743d21
Avinash Kumar <ak7797405@gmail.com>
2024-12-29 16:04:21 +0530

    fixed deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure Vercel knows where to find the build files
+  },
 });
