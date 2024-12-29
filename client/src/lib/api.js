08a272d23e2f96505c50afe178762ab05ba1877f
Avinash Kumar <ak7797405@gmail.com>
2024-12-29 15:48:44 +0530

    changed api url

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://travelapp-avinash.onrender.com/api',
 });
 
 // Interceptor to add Authorization token to headers
